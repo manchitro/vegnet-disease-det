4397bfccbb67dca86d4ec7d2a68ea0daee08ed22
Sazid <initialsaremsu@gmail.com>
2023-12-06 02:26:38 +0600

    plot all available experiments

--- yolo/plot_mAPs.py
+++ yolo/plot_mAPs.py
@@ -3,23 +3,35 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import os
 
+def list_subdirectories(directories):
+    subdirs = []
+    for directory in directories:
+        if os.path.isdir(directory):
+            subdirs.extend([os.path.join(directory, d) for d in os.listdir(directory) if os.path.isdir(os.path.join(directory, d))])
+    return subdirs
+
+directories = ['../yolo_out', '../vegnet_yolo_out']
+all_subdirs = list_subdirectories(directories)
+for subdir in all_subdirs:
+    print(subdir)
+# print(all_subdirs)
 # List of csv files
-experiment_names = ['vegnet_yolov8s_original_dataset', 
-                    'vegnet_yolov8s_aug_200_all_cropped', 
-                    'vegnet_yolov8s_aug_200_210_all_cropped',
-                    'vegnet_yolov8s_200_210_untrained',
-                    'vegnet_yolov8s_200_210_untrained_cont',
-                    'vegnet_yolov8m_original2',
-                    'vegnet_yolov8m_200',
-                    'vegnet_yolov8m_200_210',
-                    'vegnet_yolov8m_200_210_220'
-                    ]
-csv_files = [os.path.join('../yolo_out', experiment, 'results.csv') for experiment in experiment_names]
-print('cwd:', os.getcwd())
+# experiment_names = ['vegnet_yolov8s_original_dataset', 
+#                     'vegnet_yolov8s_aug_200_all_cropped', 
+#                     'vegnet_yolov8s_aug_200_210_all_cropped',
+#                     'vegnet_yolov8s_200_210_untrained',
+#                     'vegnet_yolov8s_200_210_untrained_cont',
+#                     'vegnet_yolov8m_original2',
+#                     'vegnet_yolov8m_200',
+#                     'vegnet_yolov8m_200_210',
+#                     'vegnet_yolov8m_200_210_220'
+#                     ]
+csv_files = [os.path.join(exp, 'results.csv') for exp in all_subdirs]
 
 # Plotting the mAP progression for each csv file
 fig, ax = plt.subplots(figsize=(10, 6))
-for file, label in zip(csv_files, experiment_names):
+for file, label in zip(csv_files, all_subdirs):
+    label = os.path.basename(label)
     df = pd.read_csv(file)
     mAP_values = df[df.columns[6]].tolist()
     max_mAP = max(mAP_values)
