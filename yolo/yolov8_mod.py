4f8d07bbfbaaa124355fd1c0721d6c7d3be278a3
Sazid <initialsaremsu@gmail.com>
2023-12-06 00:31:58 +0600

    yolo py script

---
+++ yolo/yolov8_mod.py
@@ -0,0 +1,41 @@
+# %%
+from ultralytics import YOLO
+import hiddenlayer as hl
+import os
+
+print(os.getcwd())
+model_name = 'yolov8s'
+exp_name = 'original_from_pt'
+model = YOLO(f'./{model_name}.yaml')
+model.load(f'./{model_name}.pt')  # build from YAML and transfer weights
+# print(model)
+
+results = model.train(
+	data = './vegnet_yolo.yaml',
+	epochs = 1000,
+	batch = 8,
+	imgsz = 256,
+	device = 0,
+	workers = 16,
+	optimizer = 'Adam',
+	pretrained = False,
+	val = True,
+	plots = True,
+	save = True,
+	save_period = 10,
+	show = True,
+	patience = 50,
+	lr0 = 0.00001,
+	lrf = 0.001,
+	fliplr = 0.0,
+	amp = False,
+	exist_ok = True,
+	name = f'{model_name}_{exp_name}',
+	project = '../vegnet_yolo_out'
+)
+
+print(results)
+path = model.export(format="onnx")  # export the model to ONNX format
+print(path)
+
+
